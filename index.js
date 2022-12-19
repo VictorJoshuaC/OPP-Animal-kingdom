06775fa1fef7afcdc7f846354c067e800a2cbd99
victor joshua <vickyjoshua49@gmail.com>
2022-12-19 16:06:59 +0100

    ara

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-
-        //    Modeling the Classification of the Animal Kingdom in JavaScript
-        //    This project is a JavaScript class that models the classification of the animal kingdom. 
-        //    It applies all four principles of object-oriented programming (OOP): encapsulation, abstraction, inheritance, and polymorphism.
-
+ 
 class AnimalKingdom {
     constructor(name,type, blooded, backbone) {
         this.name = name;
@@ -24,23 +20,23 @@ class AnimalKingdom {
     
     // inheritance
     static createFish(type,  blooded, backbone) {
-        return new AnimalKingdom("Shark",type,  blooded, backbone);
+        return new AnimalKingdom("fish",type,  blooded, backbone);
     }
 
     static createAmphibia(type,  blooded, backbone) {
-        return new AnimalKingdom("Frog",type,  blooded, backbone);
+        return new AnimalKingdom("toad",type,  blooded, backbone);
     }
 
     static createReptile(type,  blooded, backbone) {
-        return new AnimalKingdom("Tortoise",type,  blooded, backbone);
+        return new AnimalKingdom("lizard",type,  blooded, backbone);
     }
 
     static createAves(type,  blooded, backbone) {
-        return new AnimalKingdom("Bird",type,  blooded, backbone);
+        return new AnimalKingdom("eagle",type,  blooded, backbone);
     }
 
     static createMammal(type,  blooded, backbone) {
-        return new AnimalKingdom("Cat",type,  blooded, backbone);
+        return new AnimalKingdom("lion",type,  blooded, backbone);
     }
     
 
@@ -53,18 +49,18 @@ class AnimalKingdom {
         } else if (this.type === "Reptile") {
             return "I crawl";
         } else if (this.type === "Arthropods") {
-            return "i hav a high degree of movement and flexibility";
+            return "i hav  flexibility";
         } else if (this.type === "Fish") {
             return "I swim";
         
         } else if (this.type === "Amphibia") {
-            return "i do Toad movement";
+            return "i move and jump";
         
         } else if (this.type === "Mammal") {
             return "I walk";
         
         } else {
-            return "I move in some way";
+            return "I move in a  way";
         }
     }
     
@@ -74,7 +70,7 @@ class AnimalKingdom {
     }
 }
     
-    const arthropods = new AnimalKingdom("Butterfly", "Arthropods", "warm","false");
+    const arthropods = new AnimalKingdom("spider", "Arthropods", "warm","false");
     console.log(arthropods.toString()); 
     console.log(arthropods.move(), "\n\n"); 
 
